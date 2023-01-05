7db0e612996589523166bf70210b7c57ae38df6a
nina okumura <nina.okumura@spark.co.nz>
2023-01-05 20:49:48 +1300

    add checkbox component + add-ons page

---
+++ src/components/CheckboxCard.tsx
@@ -0,0 +1,101 @@
+import { ChangeEvent } from "react";
+import styled from "styled-components";
+import checkIcon from "../assets/icon-checkmark.svg";
+
+type Props = {
+  checked: boolean;
+  value?: string;
+  id?: string;
+  onChange: (evt: ChangeEvent<HTMLInputElement>) => void;
+  title: string;
+  description: string;
+  price: string;
+};
+
+const Wrapper = styled.div`
+  border: 1px solid ${({ theme }) => theme.colors["light-gray"]};
+  padding: 20px;
+  padding-left: 0;
+  border-radius: 10px;
+  max-width: 500px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const InputCheckbox = styled.input<{
+  checked?: boolean;
+}>`
+  opacity: 0;
+`;
+
+const CustomCheckbox = styled.div<{
+  checked?: boolean;
+}>`
+  border: 1px solid ${({ theme }) => theme.colors["light-gray"]};
+  background-color: ${(props) =>
+    props.checked ? "hsl(243, 100%, 62%)" : "white"};
+  width: 20px;
+  height: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+`;
+
+const Label = styled.label`
+  display: flex;
+  gap: 15px;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const CardTitle = styled.h5`
+  color: ${({ theme }) => theme.colors["marine-blue"]};
+`;
+
+const CardDescription = styled.p`
+  font-size: 13px;
+  padding-top: 4px;
+  color: ${({ theme }) => theme.colors["cool-gray"]};
+`;
+
+const Price = styled.p`
+  font-size: 13px;
+  color: ${({ theme }) => theme.colors["purplish-blue"]};
+`;
+
+const Icon = styled.img<{
+  checked?: boolean;
+}>`
+  color: ${(props) => (props.checked ? "white" : "hidden")};
+`;
+
+const CheckboxCard = (props: Props) => {
+  return (
+    <Wrapper>
+      <Label>
+        <InputCheckbox
+          type="checkbox"
+          value={props.value}
+          id={props.id}
+          onChange={props.onChange}
+        />
+
+        <CustomCheckbox checked={props.checked}>
+          <Icon src={checkIcon} alt="check icon" checked={props.checked} />
+        </CustomCheckbox>
+
+        <div>
+          <CardTitle>{props.title}</CardTitle>
+          <CardDescription>{props.description}</CardDescription>
+        </div>
+      </Label>
+      <div>
+        <Price>{props.price}</Price>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default CheckboxCard;
--- src/pages/add-ons.tsx
+++ src/pages/add-ons.tsx
@@ -1,14 +1,48 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import MainLayout from "../layouts/MainLayout";
 import styled from "styled-components";
+import CheckboxCard from "../components/CheckboxCard";
+import MainLayout from "../layouts/MainLayout";
 
 type Props = {};
+
+const ADD_ONS = [
+  {
+    title: "Online service",
+    description: "Access to multiplayer games",
+    pricePerMonth: "+$1/mo",
+    pricePerYear: "+10/yr",
+  },
+  {
+    title: "Larger storage",
+    description: "Extra 1TB of cloud save",
+    pricePerMonth: "+$2/mo",
+    pricePerYear: "+20/yr",
+  },
+  {
+    title: "Customizable profile",
+    description: "Custom theme on your profile",
+    pricePerMonth: "+$2/mo",
+    pricePerYear: "+20/yr",
+  },
+];
 
 const Wrapper = styled.div`
   width: 100%;
 `;
 
+const ListItem = styled.li`
+  list-style: none;
+`;
+
+const ListContainer = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
 const AddOns = (props: Props) => {
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+
   const navigate = useNavigate();
 
   const handlePreviousClick = () => {
@@ -25,7 +59,28 @@ const AddOns = (props: Props) => {
       onNextClick={handleNextClick}
       onPreviousClick={handlePreviousClick}
     >
-      <Wrapper>ADD ONS</Wrapper>
+      <Wrapper>
+        <ListContainer>
+          {ADD_ONS.map((addOn) => (
+            <ListItem key={addOn.title}>
+              <CheckboxCard
+                checked={selectedOptions.includes(addOn.title)}
+                value={addOn.title}
+                onChange={(evt) =>
+                  setSelectedOptions((prev) =>
+                    evt.target.checked
+                      ? prev.concat(addOn.title)
+                      : prev.filter((option) => option !== addOn.title)
+                  )
+                }
+                title={addOn.title}
+                description={addOn.description}
+                price={addOn.pricePerMonth}
+              />
+            </ListItem>
+          ))}
+        </ListContainer>
+      </Wrapper>
     </MainLayout>
   );
 };

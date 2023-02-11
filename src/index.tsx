8e81548df14c858645550ad4e4b5f1841be43b41
nina okumura <nina.okumura@spark.co.nz>
2023-02-12 08:48:02 +1300

    attempt to fix gh-pages setup

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <HashRouter>
+      <HashRouter basename="/">
         <App />
       </HashRouter>
     </ThemeProvider>
